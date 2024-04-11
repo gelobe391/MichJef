f4d74545a5a2448cc380a2af332a69e6887c9bc9
w4ffle123 <118687357+w4ffle123@users.noreply.github.com>
2024-04-11 20:52:35 +0800

    final

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'studentorg',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'projectsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.joins(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -67,6 +68,12 @@ TEMPLATES = [
         },
     },
 ]
+
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIR = (
+    BASE_DIR / 'static',
+)
 
 WSGI_APPLICATION = 'projectsite.wsgi.application'
 
