1491134ff5a55c2d9b6de10252be51b2f68cc202
w4ffle123 <118687357+w4ffle123@users.noreply.github.com>
2024-04-11 23:52:04 +0800

    joke lang yung kanina na final

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from studentorg.models import Organization
 
-class HomePageview(ListView):
+class HomePageView(ListView):
     model = Organization
     context_object_name = 'home'
     template_name = "home.html"
