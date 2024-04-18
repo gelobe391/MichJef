ccedf8a249c6f4335ce1d08cc64454c9146ddf9e
Xcaliburr-AT <2020120085@psu.palawan.edu.ph>
2024-04-18 15:33:16 +0800

    Fixing more errors

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -1,11 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path
-from studentorg.views import HomePageView, OrganizationList 
+from studentorg.view import HomePageView, OrganizationList 
 from studentorg import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', views.HomePageView.as_view(), name='home'),
-    path('organization_list', OrganizationList.as_View(), name='organization-list'),
+    path('organization_list', OrganizationList.as_view(), name='organization-list'),
 ]
