3b519c3242a9307458557e02c9202b5d77240ffc
Xcaliburr-AT <2020120085@psu.palawan.edu.ph>
2024-05-31 21:56:57 +0800

    bugfix

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -190,8 +190,8 @@ class ProgramDeleteView(DeleteView):
     success_url = reverse_lazy('program-list')
 
 class ChartView(ListView):
-    template_name = &#39;chart.html&#39;
-    
+    template_name = 'chart.html'
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
